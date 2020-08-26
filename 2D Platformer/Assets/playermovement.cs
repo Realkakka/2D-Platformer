a1195bbb0238ed4e32fffc908ca8f9272fee4722
Realkakka <thomas.rickstrom@live.com>
2020-08-27 01:18:10 +0300

    2D-platformer

---
+++ 2D Platformer/Assets/playermovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Input.GetAxisRaw("Horizontal")
+
+    }
+    void FixedUpdate ()
+    {
+        //move our character
+        controller.move(horizontalMove;, false, false);
+
+    }
+
+}
